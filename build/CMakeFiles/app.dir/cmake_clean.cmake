5f7084011d1ce302941b2710f6560e04cb5cb3f6
34262 <772834262@qq.com>
2025-03-19 17:33:42 +0800

    make

---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/app"
+  "../bin/app.pdb"
+  "CMakeFiles/app.dir/src/addd.cpp.o"
+  "CMakeFiles/app.dir/src/addd.cpp.o.d"
+  "CMakeFiles/app.dir/src/main.cpp.o"
+  "CMakeFiles/app.dir/src/main.cpp.o.d"
+  "CMakeFiles/app.dir/src/subb.cpp.o"
+  "CMakeFiles/app.dir/src/subb.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
